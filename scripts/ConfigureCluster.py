154deaf54e2430e9905c6827ad59a54d2f57c7c6
Scott R Mead <scottm@openscg.com>
2015-06-02 00:51:38 -0400

    Quite a few changes + new ui
    * Added multiple options
    * Added config file with multi-cluster support
    * Added start / stop / restart functionality
    * Fixed output from remote commands
    * Moved all cluster temp data to isolated directory
    * Refactor of code to remove duplicate function calls

--- scripts/ConfigureCluster.py
+++ scripts/ConfigureCluster.py
@@ -2,83 +2,123 @@
 
 import subprocess
 import sys
+import os
+import yaml
+import argparse
 
-# Hosts definition, this is a python list of tuples
-# LIST = [('hostuser@host1name','host1.internal.ip.addr'),
-#         ('hostuser@host2name','host2.internal.ip.addr'),
-#         etc... ]
+def exec_ssh(COMMAND,thehost):
+    ssh = subprocess.Popen(["ssh","-t", "%s" % thehost, COMMAND],
+                           shell=False,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE)
+    #result = ssh.stdout.readlines()
+#    print('\t\t'+ str(ssh.stdout.read()))
+    for line in ssh.stdout.readlines():
+        sys.stdout.write('\t\t'+line)
 
-HOSTS=[('ec2-user@ec2-54-146-215-30.compute-1.amazonaws.com','10.136.20.218'),
-       ('ec2-user@ec2-54-234-22-242.compute-1.amazonaws.com','10.138.15.49'),
-       ('ec2-user@ec2-54-234-29-0.compute-1.amazonaws.com','10.140.210.32')]
+def exec_scp(FILE,thehost):
+    cathost = subprocess.Popen(["scp", FILE,"%s:" % thehost],
+                               shell=False,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    print(cathost.stdout.readlines()) 
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument ('ACTION',choices=['provision','start','stop','restart','status'])
+parser.add_argument ( '-c','--config',help='Path to config file',action='store',dest='config')
+parser.add_argument ( '-g','--group',help='Name of the server group',action='store',dest='group')
+
+opts = parser.parse_args()
+
+print(opts.config)
+print(opts.group)
+
+with open(opts.config, 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+if not opts.group in cfg:
+    print("Group " + opts.group + " Not found")
+    exit(1)
+
+HOSTS=[]
+
+for host in cfg[opts.group]:
+    if host == 'username' :
+        username=cfg[opts.group][host]
+    else:
+    # print(cfg[opts.group][host]['hostname'])
+        HOSTS.append((cfg[opts.group][host]['hostname'],cfg[opts.group][host]['int_ip'],host))
+
+# Sort by node index
+HOSTS.sort(key=lambda tup: tup[2])
+print(HOSTS)
+
+#       ('ec2-user@ec2-54-226-106-186.compute-1.amazonaws.com','10.151.47.49')]
 
 # Build HOSTS file for this cluster
+
+scriptdir = os.path.dirname(os.path.realpath(__file__))
+
+print(scriptdir)
+
+try:
+    os.chdir ( scriptdir + '/' + opts.group )
+except IOError: 
+    os.mkdir ( scriptdir + '/' + opts.group )
 
 counter=1
 
 fo = open ( "hosts","wb")
 for HOST in HOSTS:
     # Get the hostname
-    thehost=str(HOST[0].split('@')[1])
+    #thehost=str(HOST[0].split('@')[1])
+    thehost=username + '@' + str(HOST[0])
 
     # Write this host to the hosts file
-    fo.write(str(HOST[1]) + " " + thehost + ' ' + 'cluster' + str(counter) + '\n')
+    #fo.write(str(HOST[1]) + " " + thehost + ' ' + 'cluster' + str(counter) + '\n')
+    fo.write(str(HOST[1]) + " " + 'cluster' + str(HOST[2].split('node')[1]) + ' # ' + thehost + '\n')
 
     # Writeout the zookeeper id file
     h = open (thehost +".myid","wb")
-    h.write(str(counter))
+    h.write(str(HOST[2].split('node')[1]))
     h.close()
      
     counter+=1
 fo.close()
 
 # Ports are handled in ~/.ssh/config since we use OpenSSH
-COMMANDS=['sudo yum -y localinstall https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chef-12.0.3-1.x86_64.rpm',
-          'sudo yum -y install git',
+COMMANDS=['sudo bash /home/ec2-user/placehosts.sh %s',
+          'which chef-solo || sudo yum -y localinstall https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chef-12.0.3-1.x86_64.rpm',
+          'which git || sudo yum -y install git',
           'cd /opt/storm && sudo git pull',
-          'cd /opt/ && sudo git clone https://github.com/scott-mead/storm.git',
+          'cd /opt/ || sudo git clone https://github.com/scott-mead/storm.git',
           'cd /opt/storm/chef-repo && sudo chef-solo -c solo.rb -j solo.json']
 
 for HOST in HOSTS:
-    thehost=str(HOST[0].split('@')[1])
+    thehost=username + '@' + HOST[0]
 
-    print("Now connecting to: " + str(HOST[0]).split('@')[1])
+    print("Now connecting to: " + HOST[2] + " " + thehost)
 
-    cathost = subprocess.Popen(["scp", "hosts","%s:" % str(HOST[0])],
-                               shell=False,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    print(cathost.stdout.readlines()) 
+    if opts.ACTION == 'provision':
+        exec_scp(scriptdir + "/hosts",thehost)
+        exec_scp(scriptdir + "/placehosts.sh",thehost)
+        exec_scp(thehost+".myid",thehost)
 
-    cathost = subprocess.Popen(["scp", "placehosts.sh","%s:" % str(HOST[0])], 
-                               shell=False,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    print(cathost.stdout.readlines()) 
+        for COMMAND in COMMANDS:
+            if 'placehosts.sh' in COMMAND:
+                COMMAND = COMMAND % thehost
+        
+            print("\tNow Executing: " + COMMAND)
+            exec_ssh(COMMAND,thehost)
 
-    cathost = subprocess.Popen(["scp", thehost+".myid","%s:" % str(HOST[0])], 
-                               shell=False,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    print(cathost.stdout.readlines()) 
-
-    cathost = subprocess.Popen(["ssh","-t", "%s" % str(HOST[0]), "sudo bash /home/ec2-user/placehosts.sh"],
-                               shell=False,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    print(str(cathost.stdout.readlines())) 
-
-    for COMMAND in COMMANDS:
+    elif opts.ACTION=='status':
+        exec_ssh ('ps aux | egrep \'supervisor|zookeeper|storm\'',thehost)
+            
+    else:
+        COMMAND="sudo /etc/init.d/supervisord " + opts.ACTION
         print("\tNow Executing: " + COMMAND)
-        ssh = subprocess.Popen(["ssh","-t", "%s" % str(HOST[0]), COMMAND],
-                               shell=False,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-        #result = ssh.stdout.readlines()
-        print(str(ssh.stdout.readlines()))
-'''        if result == []:
-            error = ssh.stderr.readlines()
-            print >>sys.stderr, "ERROR: %s" % error
-        else:
-            print result'''
+        exec_ssh(COMMAND,thehost)
+
+os.chdir(scriptdir)
 
--- scripts/placehosts.sh
+++ scripts/placehosts.sh
@@ -4,23 +4,26 @@
 # Read each line in the cluster config 
 cat /home/ec2-user/hosts | sed '/^$/d' | while read line
 do
-    printf "Checking if $line exists already\n"
-
     grep -q "$line" /etc/hosts
 
     if [ "$?" -ne 0 ] 
     then
-        printf "\t$line not configured, adding...."
+        printf "Adding $line "
         sudo echo $line >> /etc/hosts
         printf " DONE\n"
-    else
-        printf "\t$line already configured\n"
     fi
 done
 
-sudo diff /home/ec2-user/*.myid /tmp/zookeeper/myid
 
+test -d /tmp/zookeeper
 if [ "$?" -ne 0 ] 
 then
-    sudo cp /home/ec2-user/*.myid /tmp/zookeeper/myid
+    sudo mkdir /tmp/zookeeper
 fi
+
+diff /home/ec2-user/$1.myid /tmp/zookeeper/myid
+if [ "$?" -ne 0 ] 
+then
+    sudo cp /home/ec2-user/$1.myid /tmp/zookeeper/myid
+fi
+
